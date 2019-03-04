f3398c1b0c72c6d26a5822b115f67146ce7e81d2
Eddie Barraco <e.barraco@ubitransport.com>
2019-03-04 17:09:27 +0100

    Give a corrective command fix

--- gitconfig.d/template/bin/lint.sh
+++ gitconfig.d/template/bin/lint.sh
@@ -43,15 +43,19 @@ fi
 
 git diff --cached --name-status | grep ".$extension$" | sed 's/^\w\s//' | while read -r file
 do
-	tmp_file=$(mktemp)
-	git show ":$file" > "$tmp_file"
+	staged_file=$(mktemp --suffix=-STAGED)
+	git show ":$file" > "$staged_file"
+	tmp_file=$(mktemp --suffix=-FIXED)
+	cat "$staged_file" > "$tmp_file"
 
-	$linter $linter_options $linter_dry_run "$tmp_file" >> /dev/null 2>&1
+	$linter $linter_options $linter_dry_run "$tmp_file" >> /dev/null
 
 	if [ $? -ne 0 ]
 	then
 		$linter $linter_options "$tmp_file" >> /dev/null 2>&1
-		diff -u "$file" "$tmp_file"
+		diff -u "$staged_file" "$tmp_file"
+		echo "Correct this:"
+		echo "diff -u \"$staged_file\" \"$tmp_file\" | patch \"$file\""
 		exit 1
 	fi
 done
