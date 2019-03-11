aa83b08d2d075e56133037809f678e7453972d49
Eddie Barraco <contact@eddiebarraco.fr>
2019-03-11 18:37:37 +0100

    Only listen INBOX emails

--- bin/listen-emails.sh
+++ bin/listen-emails.sh
@@ -19,7 +19,7 @@ function notify {
 
 /usr/bin/notify-send -u low "System" "Listening for emails"
 
-for mail_dir in $HOME/.mails/*/*/new/
+for mail_dir in $HOME/.mails/*/INBOX/new/
 do
 	echo "Watching $mail_dir"
 	while mail_name=$(/usr/bin/inotifywait -q -e create -e moved_to --format '%f' "$mail_dir")
