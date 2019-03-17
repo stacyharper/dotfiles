4d16c96805fc00642a31e72db88f8dc36438feb3
Eddie Barraco <contact@eddiebarraco.fr>
2019-03-17 12:21:43 +0100

    Attach if work session already exist

--- bin/tmux_work_session.sh
+++ bin/tmux_work_session.sh
@@ -2,8 +2,11 @@
 
 session_name="work_session"
 
-tmux new-session -s "$session_name" -d "newsboat" || exit 1
-tmux split-window -t "$session_name" -h "mutt"
-tmux split-window -t "$session_name.1" "watch khal"
+tmux new-session -s "$session_name" -d "newsboat"
+if [ $? -eq 0 ]
+then
+	tmux split-window -t "$session_name" -h "mutt"
+	tmux split-window -t "$session_name.1" "watch khal"
+fi
 
 tmux attach-session -t "$session_name.3"
