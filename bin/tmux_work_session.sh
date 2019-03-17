fe31488bb02dce0d773e54dddd1d297c3fbf2548
Eddie Barraco <contact@eddiebarraco.fr>
2019-03-17 12:19:16 +0100

    Focus automaticaly mutt

--- bin/tmux_work_session.sh
+++ bin/tmux_work_session.sh
@@ -6,4 +6,4 @@ tmux new-session -s "$session_name" -d "newsboat" || exit 1
 tmux split-window -t "$session_name" -h "mutt"
 tmux split-window -t "$session_name.1" "watch khal"
 
-tmux attach-session -t "$session_name"
+tmux attach-session -t "$session_name.3"
