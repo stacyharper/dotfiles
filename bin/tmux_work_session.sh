bc538a464ca95bddef02d3372eab343c2bb4ae1c
Eddie Barraco <e.barraco@ubitransport.com>
2019-03-18 10:50:21 +0100

    Watch khal with colors

--- bin/tmux_work_session.sh
+++ bin/tmux_work_session.sh
@@ -6,7 +6,7 @@ tmux new-session -s "$session_name" -d "newsboat"
 if [ $? -eq 0 ]
 then
 	tmux split-window -t "$session_name" -h "mutt"
-	tmux split-window -t "$session_name.1" "watch khal"
+	tmux split-window -t "$session_name.1" "watch --color 'khal --color'"
 fi
 
 tmux attach-session -t "$session_name.3"
