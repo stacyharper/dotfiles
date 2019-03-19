920e3ef8980c8334ad2ab080bab93f8e7a46e9e5
Eddie Barraco <e.barraco@ubitransport.com>
2019-03-19 13:23:12 +0100

    qutebrowser use xterm

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -67,4 +67,4 @@ config.bind('<z><p><l>', 'spawn --userscript qute-pass --password-only')
 config.bind('<z><u><l>', 'spawn --userscript qute-pass --username-only')
 config.bind('gT', 'tab-prev')
 config.bind('gt', 'tab-next')
-c.editor.command = ['xdg-open', '{file}']
+c.editor.command = ['xterm', '-e', 'vim', '{file}', '-c', 'normal {line}G{column0}l']
