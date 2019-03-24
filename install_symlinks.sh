6dc24b48ec9b463619d35771f4325f93b89ffb44
Eddie Barraco <contact@eddiebarraco.fr>
2019-03-24 09:29:20 +0100

    Fix newsboat color config

--- install_symlinks.sh
+++ install_symlinks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ln -s ~/Configuration/newsboat/urls ~/.newsboat/urls
+ln -s ~/Configuration/newsboat/urls ~/.config/newsboat/urls
 ln -s ~/Configuration/qutebrowser/urls ~/.config/qutebrowser/bookmarks/urls
